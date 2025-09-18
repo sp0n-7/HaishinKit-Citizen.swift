66299813e80e39d012ccb5acf9cff048de1684d3
Armen Mkrtchian <Armenm@users.noreply.github.com>
2025-09-18 03:14:01 -0400

    Update AudioNode.swift

--- HaishinKit/Sources/Mixer/AudioNode.swift
+++ HaishinKit/Sources/Mixer/AudioNode.swift
@@ -134,10 +134,9 @@ final class MixerNode: AudioNode {
         componentFlags: 0,
         componentFlagsMask: 0)
 
-    @_optimize(none)
-    @inline(never)
     init(format: AVAudioFormat) throws {
-        try super.init(description: &mixerComponentDescription)
+        var desc = mixerComponentDescription
+        try super.init(description: &desc)
     }
 
     func update(inputCallback: inout AURenderCallbackStruct, bus: UInt8) throws {
